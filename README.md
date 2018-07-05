cb7c40cafff034078419d604e5192cae624ef05a
Andy C <590100+andrewchung@users.noreply.github.com>
2018-07-05 13:18:50 -0400

    working files updated with template code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# project
+# Team Super Duper's Project
+This is a starter template for the team project - July 5, 2018 
+UofT Coding Bootcamp
+
+# Project
+
++ initial files and directories setup with template code
++ group Gmail setup as "superduper.bootcamp@gmail.com" -- see ref sheet for password
++ Firebase project setup as "superduper-team-project" -- access code inserted into app.js
++ new GitHub home is "superduper-bootcamp" -- see ref sheet for password
+
---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  // Initialize Firebase with superduper-team-project credentials
+  var config = {
+    apiKey: "AIzaSyBFIjYm2V3clPAGbA2I4Ak_XaLo4587s6s",
+    authDomain: "superduper-team-project.firebaseapp.com",
+    databaseURL: "https://superduper-team-project.firebaseio.com",
+    projectId: "superduper-team-project",
+    storageBucket: "superduper-team-project.appspot.com",
+    messagingSenderId: "144152945917"
+  };
+  firebase.initializeApp(config);
+})
